53e27bb09e5c21ea2139f4881d3af761148058a2
jcgdhcj <fordiscordinvites2nd@gmail.com>
2026-08-30 21:12:19 +0530

    bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -141,7 +141,7 @@ export const botConfig = {
     },
     footer: {
       // Default footer text used in bot embeds.
-      text: "Super Bot",
+      text: "Mega Bot",
       // Footer icon URL (null = no icon).
       icon: null,
     },
