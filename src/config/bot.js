ef72148fed29fb239a0377d05a96b49f52f46a38
jcgdhcj <fordiscordinvites2nd@gmail.com>
2026-09-03 16:33:48 +0530

    bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         name: "Custom Status", // required by Discord API, not shown in the client
-        state: "Eating",     // this is what people actually see
+        state: "Stacking👀",     // this is what people actually see
         type: 4,               // Custom
       },
     ],
